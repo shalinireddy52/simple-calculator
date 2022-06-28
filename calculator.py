a60a1df6f5ab39c2ccdb0fee3d49e5d075a3bc58
Shalini <bodeshalinireddy@gmail.com>
2022-06-28 12:00:00 -0500

    added calculator methods

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# simple-calculator/calculator.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
